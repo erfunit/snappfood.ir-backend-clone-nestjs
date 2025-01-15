fb934b4bb28736dcec9302b10e33091368a2b4be
Mohammad Erfun Ghodoosi <erfunghodoosi@gmail.com>
2025-01-15 21:19:30 +0330

    supplier auth guard

--- src/common/types/user.d.ts
+++ src/common/types/user.d.ts
@@ -1,9 +1,10 @@
+import { SupplierEntity } from 'src/modules/supplier/entities/supplier.entity';
 import { UserEntity } from 'src/modules/users/entity/user.entity';
 
 declare global {
   namespace Express {
     interface Request {
-      user?: UserEntity;
+      user?: UserEntity | SupplierEntity;
     }
   }
 }
