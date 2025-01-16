aa2e3c0b7d0e235f229c7d3e9983e317334a1856
Mohammad Erfun Ghodoosi <erfunghodoosi@gmail.com>
2025-01-16 21:57:24 +0330

    basket dto

---
+++ src/modules/basket/dto/basket.dto.ts
@@ -0,0 +1,19 @@
+import { ApiProperty } from '@nestjs/swagger';
+import { Transform } from 'class-transformer';
+import { IsNotEmpty, IsNumber, IsString } from 'class-validator';
+
+export class AddToBasketDto {
+  @ApiProperty()
+  @Transform((value) =>
+    typeof value === 'string' ? parseInt(value, 10) : value,
+  )
+  @IsNumber()
+  foodId: number;
+}
+
+export class DiscountBasketDto {
+  @ApiProperty()
+  @IsString()
+  @IsNotEmpty()
+  code: string;
+}
